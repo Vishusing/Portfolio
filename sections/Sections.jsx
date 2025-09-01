192e59c6471446be2000c7bc9ee09971b272c016
Vishusing <vs299901@gmail.com>
2025-09-01 15:01:39 +0530

    Added animation to my app

--- sections/Sections.jsx
+++ sections/Sections.jsx
@@ -1,15 +1,42 @@
+import * as motion from "motion/react-client"
+
 import Hero from './Hero'
 import About from './About'
 import Skills from './Skills'
 import Contact from './Contact'
 
+
+function AnimatedSection({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2 }}
+      viewport={{ once: true, amount: 0.3 }}
+      className="mb-20"
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+
 export default function Sections() {
+  
   return (
     <>
-      <Hero />
-      <About />
-      <Skills />
-      <Contact />
+      <AnimatedSection>
+        <Hero />
+      </AnimatedSection>
+      <AnimatedSection>
+        <About />
+      </AnimatedSection>
+      <AnimatedSection>
+        <Skills />
+      </AnimatedSection>
+      <AnimatedSection>
+        <Contact />
+      </AnimatedSection>
     </>
   )
 }
